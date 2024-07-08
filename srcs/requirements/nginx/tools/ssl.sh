6350606df4df22e8157b7b96d9c854c4fe00da3b
angelasoler <manzanaembicicleta@gmail.com>
2024-07-07 22:10:11 -0300

    ajusta nome de certificado

--- srcs/requirements/nginx/tools/ssl.sh
+++ srcs/requirements/nginx/tools/ssl.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
  
 cd ../conf && \
-openssl req -x509 -sha256 -nodes -newkey rsa:2048 -days 365 -keyout localhost.key -out localhost.crt -subj "/C=US/ST=California/L=San Francisco/O=MyCompany/OU=IT/CN=localhost"
+openssl req -x509 -sha256 -nodes -newkey rsa:2048 -days 365 -keyout asoler.42.fr.key -out asoler.42.fr.crt -subj "/C=US/ST=California/L=San Francisco/O=MyCompany/OU=IT/CN=asoler.42.fr"
